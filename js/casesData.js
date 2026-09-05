932f97797ffa8930cd62bbea445c1132449a7470
Waris Chatvichaikul <waris@Wariss-MacBook-Air.local>
2026-09-05 17:28:50 +0700

    Configure Cloudflare Pages deployment

--- js/casesData.js
+++ js/casesData.js
@@ -7,7 +7,7 @@ window.CASES_DATA = [
   { id: "case-01", caseNumber: 1, title: "Case 01", folder: "Case01", slices: { axial: 140, sagittal: 111, coronal: 108 }, views3d: { horizontal: 92, vertical: 108 } },
   { id: "case-02", caseNumber: 2, title: "Case 02", folder: "Case02", slices: { axial: 160, sagittal: 153, coronal: 148 }, views3d: { horizontal: 27, vertical: 26 } },
   { id: "case-03", caseNumber: 3, title: "Case 03", folder: "Case03", slices: { axial: 103, sagittal: 106, coronal: 99 }, views3d: { horizontal: 24, vertical: 27 } },
-  { id: "case-04", caseNumber: 4, title: "Case 04", folder: "Case04", slices: { axial: 101, sagittal: 98, coronal: 84 }, views3d: { horizontal: 32, vertical: 31 } },
+  { id: "case-04", caseNumber: 4, title: "Case 04", folder: "Case04", slices: { axial: 101, sagittal: 98, coronal: 84 }, views3d: { horizontal: 32, vertical: 30 } },
   { id: "case-05", caseNumber: 5, title: "Case 05", folder: "Case05", slices: { axial: 100, sagittal: 96, coronal: 100 }, views3d: { horizontal: 18, vertical: 26 } },
   { id: "case-06", caseNumber: 6, title: "Case 06", folder: "Case06", slices: { axial: 140, sagittal: 111, coronal: 108 }, views3d: { horizontal: 92, vertical: 108 } },
   { id: "case-07", caseNumber: 7, title: "Case 07", folder: "Case07", slices: { axial: 140, sagittal: 111, coronal: 108 }, views3d: { horizontal: 92, vertical: 108 } },
